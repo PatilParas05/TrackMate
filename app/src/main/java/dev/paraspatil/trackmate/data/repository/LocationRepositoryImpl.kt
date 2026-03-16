84d6fcdd5c7008b70240f01547e48d29237a793b
PatilParas05 <parasspatil.111@gmail.com>
2026-03-16 18:21:54 +0530

    feat: implement LocationRepository with Google Fused Location

---
+++ app/src/main/java/dev/paraspatil/trackmate/data/repository/LocationRepositoryImpl.kt
@@ -0,0 +1,106 @@
+package dev.paraspatil.trackmate.data.repository
+
+import android.annotation.SuppressLint
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import dev.paraspatil.trackmate.domain.repository.Result
+import dev.paraspatil.trackmate.domain.model.TrackingLocation
+import dev.paraspatil.trackmate.domain.repository.LocationRepository
+import jakarta.inject.Inject
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlin.collections.emptyList
+
+class LocationRepositoryImpl @Inject constructor(
+    private val fusedLocationClient: FusedLocationProviderClient
+) : LocationRepository {
+
+    private var currentLocationCallBack: LocationCallback? = null
+    private var currentUserId:String ="user_1"
+    private var currentUserName: String ="Current User"
+
+    override fun observeCurrentLocation(): Flow<Result<TrackingLocation>> =
+        callbackFlow{
+            val locationCallback = object : LocationCallback(){
+                override fun onLocationResult(result: LocationResult) {
+                    val location = result.lastLocation?:return
+                    val trackingLocation = TrackingLocation.fromAndroidLocation(
+                        location,
+                        currentUserId,
+                        currentUserName
+                    )
+                    trySend(Result.Success(trackingLocation))
+                }
+            }
+            try {
+                val locationRequest = LocationRequest.Builder(5000L)
+                    .setMinUpdateIntervalMillis(2000L)
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .build()
+
+                @SuppressLint("MissingPermission")
+                fusedLocationClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    null
+                )
+            currentLocationCallBack = locationCallback
+
+                awaitClose {
+                    fusedLocationClient.removeLocationUpdates(locationCallback)
+                }
+            }catch (e:Exception){
+                trySend(Result.Error(e))
+                close(e)
+            }
+        }
+
+    override suspend fun saveLocation(location: TrackingLocation): Result<Unit> {
+        return try {
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    override suspend fun getTeamLocations(teamId: String): Result<List<TrackingLocation>> {
+        return try {
+            Result.Success(emptyList())
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    override fun observeTeamLocation(teamId: String): Flow<Result<List<TrackingLocation>>> = callbackFlow {
+        try {
+            trySend(Result.Success(emptyList()))
+            awaitClose { }
+        } catch (e: Exception) {
+            trySend(Result.Error(e))
+        }
+    }
+
+    override suspend fun startLocationUpdates(): dev.paraspatil.trackmate.domain.repository.Result<Unit> {
+        return try {
+            dev.paraspatil.trackmate.domain.repository.Result.Success(Unit)
+        } catch (e: Exception) {
+            dev.paraspatil.trackmate.domain.repository.Result.Error(e)
+        }
+    }
+
+    override suspend fun stopLocationUpdates(): dev.paraspatil.trackmate.domain.repository.Result<Unit> {
+        return try {
+            currentLocationCallBack?.let {
+                fusedLocationClient.removeLocationUpdates(it)
+                currentLocationCallBack = null
+            }
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+}
+
--- app/src/main/java/dev/paraspatil/trackmate/domain/repository/LocationRepository.kt
+++ app/src/main/java/dev/paraspatil/trackmate/domain/repository/LocationRepository.kt
@@ -6,13 +6,13 @@ import kotlinx.coroutines.flow.Flow
 interface LocationRepository {
     fun observeCurrentLocation(): Flow<Result<TrackingLocation>>
     suspend fun saveLocation(location: TrackingLocation): Result<Unit>
-    suspend fun getTeamLocation(teamId: String): Result<List<TrackingLocation>>
+    suspend fun getTeamLocations(teamId: String): Result<List<TrackingLocation>>
     fun observeTeamLocation(teamId: String): Flow<Result<List<TrackingLocation>>>
     suspend fun startLocationUpdates(): Result<Unit>
     suspend fun stopLocationUpdates(): Result<Unit>
 }
 sealed class Result<out T>{
-    data class Success<out T>(val data:T):Result<T>()
+    data class Success<T>(val data:T):Result<T>()
     data class Error(val exception: Throwable):Result<Nothing>()
     object Loading:Result<Nothing>()
 }
