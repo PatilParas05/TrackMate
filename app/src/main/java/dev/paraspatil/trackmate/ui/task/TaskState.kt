9bf1b1e13c28b72c8168d0aa01f4101d8cd673c3
PatilParas05 <parasspatil.111@gmail.com>
2026-03-19 19:04:40 +0530

    feat:define TaskState and TaskEffect for task feature

---
+++ app/src/main/java/dev/paraspatil/trackmate/ui/task/TaskState.kt
@@ -0,0 +1,16 @@
+package dev.paraspatil.trackmate.ui.task
+
+import dev.paraspatil.trackmate.domain.model.Task
+
+data class TaskState(
+    val task: List<Task> = emptyList(),
+    val selectedTask: Task? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
+sealed class TaskEffect{
+    data class ShowMessage(val message: String): TaskEffect()
+    object TaskCreated: TaskEffect()
+    object TaskUpdated: TaskEffect()
+    object TaskDeleted: TaskEffect()
+}
