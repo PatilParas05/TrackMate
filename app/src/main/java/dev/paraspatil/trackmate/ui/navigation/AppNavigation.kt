e932884df5755c534029f5f4fc3b69b4046f4a72
PatilParas05 <parasspatil.111@gmail.com>
2026-03-22 11:51:23 +0530

    feat(ui): implement AppNavigation file

---
+++ app/src/main/java/dev/paraspatil/trackmate/ui/navigation/AppNavigation.kt
@@ -0,0 +1,67 @@
+package dev.paraspatil.trackmate.ui.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import dev.paraspatil.trackmate.ui.screens.DashboardScreen
+
+sealed class Screen(val route: String,val label: String,val icon: ImageVector) {
+    object Dashboard : Screen("dashboard", "Home", Icons.Default.Home)
+
+}
+val bottomNavItems = listOf(
+    Screen.Dashboard,
+)
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+
+                bottomNavItems.forEach { screen ->
+                    NavigationBarItem(
+                        icon = {Icon(screen.icon, contentDescription = screen.label)},
+                        label = {Text(screen.label)},
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.startDestinationId) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Dashboard.route,
+            modifier = Modifier.padding(innerPadding)
+        ){
+            composable(Screen.Dashboard.route){ DashboardScreen(navController) }
+        }
+    }
+}
